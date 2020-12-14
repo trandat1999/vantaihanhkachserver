2a89ed24b8f979d447f8071abe63c99b88f66587
trandat1999 <datnuclear1999x@gmail.com>
2020-12-14 13:58:07 +0700

    init commit

--- src/main/java/com/vantaihanhkhach/repository/IChuyenXeRepository.java
+++ src/main/java/com/vantaihanhkhach/repository/IChuyenXeRepository.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -13,19 +12,16 @@ import com.vantaihanhkhach.model.ChuyenXe;
 @Repository
 public interface IChuyenXeRepository extends JpaRepository<ChuyenXe, Long> {
 
-//	@Modifying
+
 //	@Query("select a from ChuyenXe a where month(a.ngayChay) = month(?1) and year(a.ngayChay) = year(?2)")
 //	List<ChuyenXe> findByMonth(Date date, Date date2);
 
-	@Modifying
-	@Query("select a from ChuyenXe a where EXTRACT(MONTH FROM timestamp a.ngayChay)= EXTRACT(MONTH FROM timestamp ?1) and EXTRACT(YEAR FROM timestamp a.ngayChay)= EXTRACT(YEAR FROM timestamp ?2)")
+	@Query("select a from ChuyenXe a where date_part('month',timestamp a.ngayChay)= date_part('month',timestamp ?1) and date_part('year',timestamp a.ngayChay)= date_part('year',timestamp ?2)")
 	List<ChuyenXe> findByMonth(Date date, Date date2);
 
-	@Modifying
 	@Query("select a from ChuyenXe a where a.ngayChay between ?1 and ?2")
 	List<ChuyenXe> findByBetween(Date date, Date date2);
 
-	@Modifying
 	@Query("select a from ChuyenXe a where a.ngayChay >= ?1")
 	List<ChuyenXe> findByDate(Date date);
 }
