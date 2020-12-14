7bcd2f1c5fd38e0275e02f62caf51b33f9783362
trandat1999 <datnuclear1999x@gmail.com>
2020-12-14 13:32:11 +0700

    init commit

--- src/main/java/com/vantaihanhkhach/repository/IChuyenXeRepository.java
+++ src/main/java/com/vantaihanhkhach/repository/IChuyenXeRepository.java
@@ -11,16 +11,20 @@ import org.springframework.stereotype.Repository;
 import com.vantaihanhkhach.model.ChuyenXe;
 
 @Repository
-public interface IChuyenXeRepository extends JpaRepository<ChuyenXe, Long>{
-	
+public interface IChuyenXeRepository extends JpaRepository<ChuyenXe, Long> {
+
+//	@Modifying
+//	@Query("select a from ChuyenXe a where month(a.ngayChay) = month(?1) and year(a.ngayChay) = year(?2)")
+//	List<ChuyenXe> findByMonth(Date date, Date date2);
+
 	@Modifying
-	@Query("select a from ChuyenXe a where month(a.ngayChay) = month(?1) and year(a.ngayChay) = year(?2)")
+	@Query("select a from ChuyenXe a where EXTRACT(MONTH FROM a.ngayChay::date)= EXTRACT(MONTH FROM ?1::date) and EXTRACT(YEAR FROM a.ngayChay::date)= EXTRACT(YEAR FROM ?2::date)")
 	List<ChuyenXe> findByMonth(Date date, Date date2);
-	
+
 	@Modifying
 	@Query("select a from ChuyenXe a where a.ngayChay between ?1 and ?2")
 	List<ChuyenXe> findByBetween(Date date, Date date2);
-	
+
 	@Modifying
 	@Query("select a from ChuyenXe a where a.ngayChay >= ?1")
 	List<ChuyenXe> findByDate(Date date);
